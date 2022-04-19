9a7f068d98d9b030a6f92dd8b6dcccb20f385edb
Nick Lopez <0xnicklopez@gmail.com>
2022-04-19 00:19:11 -0700

    Passed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(a, b) {
+let n = "";
+if (b < 0) return "ERROR";
+ for (let i = 0; i < b; i++) {
+    n += a;
+ }
 
+ return n;
 };
+
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
